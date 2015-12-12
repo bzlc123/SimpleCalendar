27e7cd49cadda5c1b4fc35fdbe7d9acd186ac2e5
yan66665@126.com <w.y12345>
2015-12-12 21:10:33 +0800

    Delete: No use file.

--- simplecalendarlibrary/src/main/java/com/yan/simplecalendar/widget/CalendarDecorator.java
+++
@@ -1,9 +0,0 @@
-package com.yan.simplecalendar.widget;
-
-import android.widget.TextView;
-
-import java.util.Calendar;
-
-public interface CalendarDecorator {
-    void decorate(TextView calendarTV, TextView decoratorTV, Calendar calendar);
-}
